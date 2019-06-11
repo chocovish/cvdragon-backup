2c0572f4f14c6dd065a5cd937c85c9f80bd8b507
Somesh_Dave <mr.someshdave@gmail.com>
2019-06-11 16:46:36 +0530

    theme color changed

--- Development/cvdragonapp_v1/lib/main.dart
+++ Development/cvdragonapp_v1/lib/main.dart
@@ -10,29 +10,9 @@ class MyApp extends StatefulWidget {
 
   // This widget is the root of your application.
   @override
-<<<<<<< HEAD
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyApp();
-=======
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
->>>>>>> 5a729bc8ce067a26e4d07111c221544ca5c1cd5e
   }
 }
 class _MyApp extends State<MyApp>{
@@ -47,7 +27,7 @@ class _MyApp extends State<MyApp>{
    
     return MaterialApp(
       theme: ThemeData( 
-        primaryColor: Colors.deepPurple,
+        primaryColor: Colors.green,
       ),
       home:Scaffold(appBar: TopMenuBar(),
         bottomNavigationBar: BottomBar(_bottompressed),

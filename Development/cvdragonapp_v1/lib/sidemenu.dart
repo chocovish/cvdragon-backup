318b56b793dbb66548ef1521435ad8d3ac4dfe21
Rohit_Agarwal <rohitagarwal17800@gmail.com>
2019-06-12 10:51:08 +0530

    corrected the Navigation.pop thing

--- Development/cvdragonapp_v1/lib/sidemenu.dart
+++ Development/cvdragonapp_v1/lib/sidemenu.dart
@@ -58,7 +58,7 @@ class _SideMenu extends State<SideMenu> {
                   trailing: Icon(Icons.arrow_forward,color: Colors.white),
                   onTap: () {
                     setState(() {
-                      Navigator.pop(context);
+                     
                       Navigator.pop(context);
                       Navigator.push(
                           context,
@@ -82,7 +82,6 @@ class _SideMenu extends State<SideMenu> {
                   trailing: Icon(Icons.arrow_forward,color: Colors.white),
                   onTap: () {
                     Navigator.pop(context);
-                    Navigator.pop(context);
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => Sections()));
                   },
@@ -101,7 +100,7 @@ class _SideMenu extends State<SideMenu> {
                   subtitle: Text('Check your Cover Letters',style: TextStyle(color: Colors.white)),
                   trailing: Icon(Icons.arrow_forward,color: Colors.white),
                   onTap: () {
-                    Navigator.pop(context);
+                   
                     Navigator.pop(context);
                     Navigator.push(
                         context,
